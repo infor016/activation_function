09bcddd9efe1dd023e4e2fef16b04412d9286cf4
infor016 <73428452+infor016@users.noreply.github.com>
2020-10-25 23:22:26 +0800

    Create sigmoid.py

---
+++ sigmoid.py
@@ -0,0 +1,7 @@
+class Sigmoid(ActivationFunction):
+    def f(self, x):
+        return np.where(x >= 0, 1 / (1 + np.exp(-x)), np.exp(x) / (1 + np.exp(x)))
+
+    def df(self, x, cached_y=None):
+        y = cached_y if cached_y is not None else self.f(x)
+        return y * (1 - y)
